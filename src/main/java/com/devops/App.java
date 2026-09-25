1c8be296f538c62151614082dde0a6415ccc0545
2022wb86571 <2022wb86571@wilp.bits-pilani.ac.in>
2026-09-25 13:43:09 +0530

    Replace greeting message with CI message

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -3,6 +3,7 @@ package com.devops;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello from DevOps Lab!");
+        //System.out.println("Hello from DevOps Lab!");
+        System.out.println("Continuous Integration using Jenkins!");
     }
 }
